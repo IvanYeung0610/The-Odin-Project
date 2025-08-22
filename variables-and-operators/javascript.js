f424bea36ab2661eb6caeb1a82cccc73ea3eaaa9
Ivan Yeung <ivanyeung0610@gmail.com>
2025-08-22 14:47:04 -0400

    Added work for variables and operators lesson

---
+++ variables-and-operators/javascript.js
@@ -0,0 +1,31 @@
+console.log("Hello World");
+
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+a = 12;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+
+const max = 57;
+const acutal = max - 13;
+const percentage = acutal / max;
+
+console.log(percentage);
