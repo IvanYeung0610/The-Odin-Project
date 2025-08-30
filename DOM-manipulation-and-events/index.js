be1ee788ed36f17683058d7c4d66c88bfbd60a82
IvanYeung0610 <ivanyeung0610@gmail.com>
2025-08-29 20:01:14 -0400

    Finish the three exercises for element creation

--- DOM-manipulation-and-events/index.js
+++ DOM-manipulation-and-events/index.js
@@ -7,3 +7,28 @@ content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
 
+// Exercises
+
+// 1.
+const para = document.createElement("p");
+para.textContent = "Hey I'm red!"
+para.style.color = "red";
+container.appendChild(para);
+
+// 2.
+const header = document.createElement("h3");
+header.textContent = "I'm a blue h3!";
+header.style.color = "blue";
+container.appendChild(header);
+
+// 3.
+const div = document.createElement("div");
+div.style.borderColor = "black";
+div.style.backgroundColor = "pink";
+const divHeader = document.createElement("h1");
+divHeader.textContent = "I'm in a div";
+const divPara = document.createElement("p");
+divPara.textContent = "ME TOO!";
+div.appendChild(divHeader);
+div.appendChild(divPara);
+container.appendChild(div);
